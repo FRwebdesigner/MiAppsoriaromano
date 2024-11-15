56ce9efd8f066621af4f02375e64bed0072d49b8
ftsoriaromano <ftsoriaromano@gmail.com>
2024-11-15 16:38:17 -0300

    new commit

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,16 @@
+
+import ItemCount from "./ItemCount"
 const ItemListContainer = ({greeting, texto}) =>{
+
+    console.log('ItemListContainer')
+    const onAdd = (cantidad) =>{
+        alert(`Agregaaste al carrito ${cantidad} productos`)
+    }
     return(
         <div>
             <h1>{greeting}</h1>
             <p>{texto}</p>
+            <itemCount stock={12} onAdd ={onAdd}/>
         </div>
     )
 }
