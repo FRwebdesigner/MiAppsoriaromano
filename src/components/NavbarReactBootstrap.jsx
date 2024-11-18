ab7af8ba026f1f355de5b1ad0c4c96b878155edb
ftsoriaromano <ftsoriaromano@gmail.com>
2024-11-18 13:51:49 -0300

    new editions

--- src/components/NavbarReactBootstrap.jsx
+++ src/components/NavbarReactBootstrap.jsx
@@ -1,6 +1,4 @@
-{
- window.$RefreshSig$ = RefreshRuntime.createSignatureFunctionForTransform;
-}
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
