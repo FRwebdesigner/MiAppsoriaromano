0267b5cca3232c09104a22c29a7e102331f2aed6
ftsoriaromano <ftsoriaromano@gmail.com>
2024-11-19 15:18:34 -0300

    new commit

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,5 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
+import  BsCart4  from "react-icons/bs"
+import  Badge  from "react-bootstrap"
 
 const CartWidget = ({counter}) => {
     
--- src/components/NavbarReactBootstrap.jsx
+++ src/components/NavbarReactBootstrap.jsx
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CartWidgetReactIcons from './CartWidgetReactIcons';
 import { NavLink } from 'react-router-dom';
 function NavbarReactBootstrap() {
   console.log('Navbar')
