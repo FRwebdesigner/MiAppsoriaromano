a6aaeaeffc12ca506ed3f9f64ca63cc61d56767d
ftsoriaromano <ftsoriaromano@gmail.com>
2024-11-19 15:11:26 -0300

    new commit

--- src/components/NavbarBootstrapHTML.jsx
+++ src/components/NavbarBootstrapHTML.jsx
@@ -1,4 +1,7 @@
-import { NavLink } from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 const NavbarBootstrapHTML = () =>{
     return(
