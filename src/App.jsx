7fe01b53ecd18813c36dd8a0e6a081f7b8a73001
ftsoriaromano <ftsoriaromano@gmail.com>
2024-12-05 18:25:06 -0300

    new editions

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Error from './components/Error';
 import { CartProvider } from './context/CartContext';
 import CartView from './components/CartView';
+import Checkout from './components/Checkout';
 
 
 function App() {
@@ -17,10 +18,11 @@ function App() {
     <BrowserRouter>
       <NavbarReactBootstrap/>
       <Routes>
-        <Route path='/' element={<ItemListContainer greeting='Bienvenidos a nuestra tienda de Mascotas'/>}/>
+        <Route path='/' element={<ItemListContainer greeting='Bienvenidos'/>}/>
         <Route path='/products/:category' element={<ItemListContainer greeting='Bienvenidos a la categoria: '/>}/>
         <Route path='/item/:id' element={<ItemDetailContainer/>}/>
         <Route path='/cart' element={<CartView/>}/>
+        <Route path='/checkout' element={<Checkout/>}/>
         <Route path='*' element={<Error/>}/>
       </Routes>
     </BrowserRouter>
--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -3,19 +3,31 @@ import { getOneProduct, getProducts } from '../mock/data'
 import ItemDetail from './ItemDetail'
 import { useParams } from 'react-router-dom'
 import Loader from './Loader'
+import { collection, doc, getDoc } from 'firebase/firestore'
+import { db } from '../services/Firebase'
 
 const ItemDetailContainer = () => {
     const [producto, setProducto]=useState({})
     const [loading, setLoading]= useState(false)
     const { id } = useParams()
-    console.log(id)
+
     useEffect(()=>{
       setLoading(true)
-        getOneProduct(id)
-        .then((res)=> setProducto(res))
-        .catch((error)=>console.log(error))
-        .finally(()=> setLoading(false))
+      const collectionProd = collection(db, "productos")
+      const docRef = doc(collectionProd,id )
+      getDoc(docRef)
+      .then((res)=> setProducto({id: res.id, ...res.data()}))
+      .catch((error)=> console.log(error))
+      .finally(()=> setLoading(false))
     },[])
+
+     useEffect(()=>{
+       setLoading(true)
+         getOneProduct(id)
+         .then((res)=> setProducto(res))
+         .catch((error)=>console.log(error))
+         .finally(()=> setLoading(false))
+     },[])
      useEffect(()=>{
          getProducts()
          .then((res)=> setProducto(res.find((item)=> item.id === '2')))
